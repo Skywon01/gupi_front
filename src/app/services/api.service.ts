8f7e971904db9d57e31fcdff4848b59e738290c9
Skywon <doublet.yann@orange.fr>
2024-09-01 16:25:55 +0200

    style chat

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import {AuthService} from "./authentification/auth.service";
 /**
  * Nom de domaine de l'api GUPI en environnement de développement local
  */
-export const apiRoot = "https://srv591226.hstgr.cloud/api"
+export const apiRoot = "92.113.31.127:8080/api"
 
 /**
  * Ici on commence la communication Http
